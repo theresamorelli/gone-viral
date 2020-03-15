d05dc0547f3f71bd98833ef48e526107b2b211fb
theresamorelli <theresa.l.morelli@gmail.com>
2020-03-14 19:52:10 -0700

    First commit

---
+++ src/components/Chart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import d3 from 'd3';
+import { render } from '@testing-library/react';
+
+class Chart extends React.Component {
+  async componentDidMount() {
+    
+  }
+
+  render() {
+  return (
+    <div className="chart">
+      Hi
+    </div>
+  );
+}
+}
+
+export default Chart;
