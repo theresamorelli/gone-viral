57f997d3a14f70888b4e766bef7ceda423111f89
theresamorelli <theresa.l.morelli@gmail.com>
2020-03-15 19:16:49 -0700

    Get data from backend, render basic info

--- api/routes/cases.js
+++ api/routes/cases.js
@@ -1,17 +1,15 @@
 var express = require("express");
 var router = express.Router();
 const csvtojson = require("csvtojson");
-const request = require('request');
+const request = require("request");
 const csvFilePath =
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid";
+  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid";
 
 router.get("/confirmed", async function(req, res, next) {
-  // const csvFilePath =
-    // "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
   csvtojson()
     .fromStream(request.get(`${csvFilePath}-Confirmed.csv`))
     .then(jsonObj => {
-      res.send(jsonObj)
+      res.send(jsonObj);
     });
 });
 
@@ -19,7 +17,7 @@ router.get("/deaths", async function(req, res, next) {
   csvtojson()
     .fromStream(request.get(`${csvFilePath}-Deaths.csv`))
     .then(jsonObj => {
-      res.send(jsonObj)
+      res.send(jsonObj);
     });
 });
 
